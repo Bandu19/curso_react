564b44bd4921b336e43faf1c4f89472fc9ffcf5f
Oscar Acuña Sóstenes <oscagustavo1961@gmail.com>
2022-06-14 14:03:48 -0500

    Variable y Constantes - Templete String

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+// Variables y Constantes
+const nombre = "Fernando";
+const apellido = "Herrera";
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+// var No se debe de usar ...
+if (true) {
+  const nombre = "Peter";
+  let valorDado = 6;
+  console.log(valorDado, nombre);
+}
+
+console.log(valorDado);
---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,3 @@
+const persona = {};
+
+console.log(persona);

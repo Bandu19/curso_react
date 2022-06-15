ac1cc412072e4148e3cb75e290fe23c53e666094
Oscar Acuña Sóstenes <oscagustavo1961@gmail.com>
2022-06-15 15:01:33 -0500

    Update Import

---
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -0,0 +1,27 @@
+// Import, export y funciones comunes de arreglos
+import { heroes } from "../data/heroes";
+
+// const getHeroeById = (id) => {
+//   return heroes.find((heroe) => {
+//     if (heroe.id === id) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// };
+
+/** OTRA MANERA  */
+
+const getHeroeById = (id) => {
+  return heroes.find((heroe) => heroe.id === id);
+};
+
+console.log(getHeroeById(2));
+
+// Find? ==> filter
+const getHeroeByOwner = (owner) => {
+  return heroes.filter((heroe) => heroe.owner === owner);
+};
+
+console.log(getHeroeByOwner("Marvel"));

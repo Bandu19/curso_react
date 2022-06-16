ca70308d648a1562a2fac9610bc171f409d7c10c
Oscar Acuña Sóstenes <oscagustavo1961@gmail.com>
2022-06-16 18:04:03 -0500

    Navigate push

--- 07-heroes-spa/src/ui/components/Navar.jsx
+++ 07-heroes-spa/src/ui/components/Navar.jsx
@@ -1,6 +1,20 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  //NAVEGACIÓN ==> HOOKS ===> useNavigate
+  /** Ofrece a que tambien limpie el localStorage
+   */
+
+  const navigete = useNavigate();
+
+  const handleLogout = () => {
+    navigete("/login", {
+      replace: true,
+    });
+  };
+
+  /** */
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -11,6 +25,7 @@ export const Navbar = () => {
         <div className="navbar-nav">
           {/* ?? ==> isActive */}
           <NavLink
+            // SI "isActive" esta en true, que si estsa en esa carpeta o no.
             className={({ isActive }) =>
               `nav-item nav-link ${isActive ? "active" : ""}`
             }
@@ -33,7 +48,13 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">Oscar</span>
-          <button className="nav-item nav-link btn">Logout</button>
+          <button
+            //
+            className="nav-item nav-link btn"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
         </ul>
       </div>
     </nav>

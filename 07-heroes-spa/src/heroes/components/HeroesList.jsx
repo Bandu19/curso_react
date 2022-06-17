1a0bcd744203b580a2977f3960b3e89764f1f1c1
Oscar Acuña Sóstenes <oscagustavo1961@gmail.com>
2022-06-16 20:15:54 -0500

    Tarjeta del Héroe

--- 07-heroes-spa/src/heroes/components/HeroesList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroesList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { HeroCard } from "./";
 import { getHeroesByPublisher } from "../helpers";
 
 export const HeroesList = ({ publisher }) => {
@@ -11,14 +11,11 @@ export const HeroesList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
         //Siempre es obligatorio poner un IDENTIFICADOR
-        <li key={hero.id}>
-          {/*  */}
-          {hero.superhero}
-        </li>
+        <HeroCard key={hero.id} {...hero} />
       ))}
-    </ul>
+    </div>
   );
 };

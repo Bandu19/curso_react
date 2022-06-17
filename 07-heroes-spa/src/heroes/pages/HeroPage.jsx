f051fbd08f66182b3ce6fc7eda964ffb1478cba8
Oscar Acuña Sóstenes <oscagustavo1961@gmail.com>
2022-06-17 11:21:50 -0500

    Estilo del Componente HeroScreen

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useParams, useNavigate } from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 export const HeroPages = () => {
@@ -10,10 +10,65 @@ export const HeroPages = () => {
   const hero = getHeroById(id);
   console.log(hero);
 
+  const navegador = useNavigate();
+
+  const onNavigateBack = () => {
+    /**
+     *
+     */
+    navegador(-1, {
+      replace: true,
+    });
+  };
+
   // SI EL HEROE NO EXISTE
   if (!hero) {
     return <Navigate to="/marvel" />;
   }
+  const heroImage = `/assets/heroes/heroes/${hero.id}.jpg`;
 
-  return <h1>{hero.superhero}</h1>;
+  return (
+    <>
+      <div className="row mt-4">
+        <h1>{hero.publisher}</h1>
+
+        <div className="col-4">
+          <img
+            src={heroImage}
+            alt={hero.superhero}
+            className="img-thumbnail"
+          ></img>
+        </div>
+
+        <div className="col-8">
+          <h3>{hero.superhero}</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <b>Alter ego:</b>
+              {hero.alter_ego}
+            </li>
+
+            <li className="list-group-item">
+              <b>Publisher: </b>
+              {hero.publisher}
+            </li>
+            <li className="list-group-item">
+              <b>First appearance: </b>
+              {hero.publisher}
+            </li>
+          </ul>
+
+          <h5 className="mt-3">Characters</h5>
+          <ul>
+            <li className="list-group-item">
+              <p>{hero.characters}</p>
+            </li>
+          </ul>
+          <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+            Regresar
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
